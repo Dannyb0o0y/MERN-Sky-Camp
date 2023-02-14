87dc2e868721708256e71d835c1d834e20d7d40b
unknown <danial.rf1999@gmail.com>
2023-02-13 21:34:27 -0500

    added code to ondex.js

--- client/src/components/Slider/index.js
+++ client/src/components/Slider/index.js
@@ -13,21 +13,26 @@ import {
 const items = [
   {
     src: '',
-    altText: 'Slide 1',
-    caption: 'Slide 1',
+    src: '',
+    altText: '',
+    caption: '',
     key: 1,
   },
+  {
+    src: '',
+    src: '',
+    altText: '',
+    caption: '',
+    key: 2,
+  },
+  {
+    src: '',
+    src: '',
+    altText: '',
+    caption: '',
+    key: 3,
+  },
 ];
-
-function Slider(args) {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
-
-  const next = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-  };
 
   const previous = () => {
     if (animating) return;
