26aa33aeb327bdd874ba8da0c92cbc28121ecb06
unknown <danial.rf1999@gmail.com>
2023-02-15 20:27:31 -0500

    made some changes to the category menu

--- client/src/components/categoryMenu/index.js
+++ client/src/components/categoryMenu/index.js
@@ -11,6 +11,7 @@ import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
+
   const { categories } = state;
 
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
@@ -43,27 +44,27 @@ function CategoryMenu() {
 
   return (
     <div>
-    <h2 style={{ textAlign: 'center' }}>Welcome to MERN Ski Camp Resort</h2>
-    <p>Taking  trip with family, friends or on your own is an excellent way to stay active.  Enjoy feeling healthier, surrounded by all the fresh air and sunshine that you'll find on the top of the mountain</p>
-    <img src={SkiCamp} alt="Ski-Camp-Picture"></img>
-        <h3>Choose a Ski-Camp Package:</h3>
-        {categories.map((item) => (
-          <button
-            key={item._id}
-            onClick={() => {
-              handleClick(item._id);
-            }}
-          >
-            {item.name}
-          </button>
-        ))}
-         <br></br>
-      <p>We also offer horse riding lessons, delicious take-away meals, heated pool, kayaking, fishing, tennis, basketball, mini golf and transportation at the resort. We also team up with other resort owners and food caterers to secure you the most affordable and best deals for you, your family and friends.  Contact us at 1800 155 155 for more information and our friendly staff would be more than happy to assist you.</p>
-      
-      <div>
+      <h2 style={{ textAlign: 'center' }}>A warm welcome to Sky camp Resort</h2>
+      <p>Taking a trip with family, friends or on your own is an excellent way to stay active.  Enjoy being feeling healthier surrounded by all the fresh air and sunshine that you'll find on the top of the mountain.</p>
      
+      <h3>Choose a Ski Package:</h3>
+      {categories.map((item) => (
+        <button 
+          key={item._id}
+          onClick={() => {
+            handleClick(item._id);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    
+      <div>
+       
       </div>
     </div>
+
+
   );
 }
 
