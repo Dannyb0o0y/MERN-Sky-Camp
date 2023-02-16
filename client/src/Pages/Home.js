0b366b92dbadd92d962bec9de5afc630e7dca2c5
unknown <danial.rf1999@gmail.com>
2023-02-15 20:50:41 -0500

    added the components to the home.js

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div className="container">
+      <Slider/>
       <CategoryMenu />
       <ProductList />
       <Cart />
