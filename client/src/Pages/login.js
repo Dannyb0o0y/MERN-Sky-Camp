cbea6e3ee91c82293938489daf4abaa65d92d92e
unknown <danial.rf1999@gmail.com>
2023-02-15 21:01:31 -0500

    fixed some of the typo issues in log in page

--- client/src/Pages/login.js
+++ client/src/Pages/login.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 
+
 function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN);
@@ -35,7 +36,7 @@ function Login(props) {
 
       <h2>Login</h2>
       <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
+        <div className="flex-row space-between my-4">
           <label htmlFor="email">Email address:</label>
           <input
             placeholder="youremail@test.com"
@@ -45,7 +46,7 @@ function Login(props) {
             onChange={handleChange}
           />
         </div>
-        <div className="flex-row space-between my-2">
+        <div className="flex-row space-between my-4">
           <label htmlFor="pwd">Password:</label>
           <input
             placeholder="******"
@@ -63,6 +64,27 @@ function Login(props) {
         <div className="flex-row flex-end">
           <button type="submit">Submit</button>
         </div>
+          <div>
+          <p><strong>We welcome you back to our Login Page.</strong></p>
+          <br></br>
+          <p>If you have forgotten your password and you will have to sign up/register with us again. </p>
+          <br></br>
+          {/* More marketing materials */}
+          <br></br>
+          <p style={{color: 'orange', fontSize:'30px'}}>Here are some of extra measures that we have in place to give you extra peace of mind for your next holiday.</p>
+          <br></br>
+          <p>2.  We'll do our very best to make sure your holiday is everything the way it should be. However, due to the constantly evolving nature of the COVID-19 crisis, we might need to change or withdraw some of the advertised accommodation facilities and resort services to keep everyone safe.  Wherever we could, you will be notified in advance but sometimes it might be at a short notice.</p>
+          <br></br>
+          <p>4.  All staff will be required to certify that they are not experiencing any Covid-19 symptoms before starting work each day.  Any staff experiencing symptoms will not attend work and appropriate actions will be made with the local regulations and our own protocols.</p>
+          <br></br>
+          <p>5. Everyone, including staff, are expected to wear face coverings when travelling on any transport to and from the resort, in line with all local regulations.  We make it mandatory that you must bring your face covering.</p>
+          <br></br>
+          <p>6. Don't forget to purchase your travel insurance.</p>
+          <br></br>
+          
+          </div>
+
+
       </form>
     </div>
   );
